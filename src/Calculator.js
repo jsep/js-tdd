2ca1611a5cf3eacb8101a3c693f35797b70a7dec
Juan Sepulveda <juan@sepulveda.do>
2015-02-10 16:22:32 -0400

    Add live example tests

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,19 +2,14 @@
  * Created by juan on 2/10/2015.
  */
 "use strict";
+
 var Calculator = (function(){
 
     var add = function(num1, num2){
         return num1 + num2;
     };
 
-    var subtract = function(num1, num2){
-        return num1 - num2;
-    };
-
     return {
-        add: add,
-        subtract: subtract
+        add: add
     }
-
 }());
